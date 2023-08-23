d535af4c3e8654b6186d1ce6c356610038a5335c
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-08-23 17:27:38 +0300

    solving-conflict

--- src/thanks-msg.js
+++ src/thanks-msg.js
@@ -1,26 +1,26 @@
 
-(() => {
-const btnOpn = document.querySelector(".open-button")
-const clsBtn = document.querySelector(".close-button")
-const thanksMsg = document.querySelector(".thanks-container")
-const backdrop = document.querySelector(".backdrop")
+// (() => {
+// const btnOpn = document.querySelector(".open-button")
+// const clsBtn = document.querySelector(".close-button")
+// const thanksMsg = document.querySelector(".thanks-container")
+// const backdrop = document.querySelector(".backdrop")
 
-const openThanks= () => {
-    thanksMsg.classList.add("is-open");
-    document.body.classList.add("no-scroll","thx-no-scroll");
-    backdrop.classList.remove("is-hidden");
-}
+// const openThanks= () => {
+//     thanksMsg.classList.add("is-open");
+//     document.body.classList.add("no-scroll","thx-no-scroll");
+//     backdrop.classList.remove("is-hidden");
+// }
 
-const closeThanks= () => {
-    thanksMsg.classList.remove("is-open");
-    document.body.classList.remove("no-scroll","thx-no-scroll");
-    backdrop.classList.add("is-hidden")
-}
+// const closeThanks= () => {
+//     thanksMsg.classList.remove("is-open");
+//     document.body.classList.remove("no-scroll","thx-no-scroll");
+//     backdrop.classList.add("is-hidden")
+// }
 
-btnOpn.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-});
+// btnOpn.addEventListener("click", () => {
+//   window.scrollTo(0, 0);
+// });
 
-btnOpn.addEventListener("click", openThanks);
-clsBtn.addEventListener("click", closeThanks);
-})();
+// btnOpn.addEventListener("click", openThanks);
+// clsBtn.addEventListener("click", closeThanks);
+// })();
